0a140983c5bec7d1d3cfe5dbb26d32bdf2d27c73
violet360 <mazedk2k@gmail.com>
2020-08-11 22:35:19 +0530

    just checking

--- ignition/urls.py
+++ ignition/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.display_list, name = 'display_list'),
 	path('<int:pk>/', views.del_task, name = 'del_task')
+	#checking
 	# path('')
 	# path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 ]
